2e6d48155634d7c1f1a1d1c016d36fab5b772019
Elfern123 <45273768+Elfern123@users.noreply.github.com>
2020-02-13 14:27:46 +0200

    Added support for Faceit demos, fixed bugs and added requirements.txt

--- csgodemohandler.py
+++ csgodemohandler.py
@@ -1,10 +1,12 @@
 import patoolib
 import requests
 import os
+import gzip
+import shutil
 from webbot import Browser
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from time import sleep
 
 config = open("config.txt", "r")
 configLines = config.read().splitlines()
@@ -14,69 +16,82 @@ noesisInUse = configLines[2]
 noesisUser = configLines[3]
 noesisPW = configLines[4]
 
+
 config.close
-from time import sleep
+webOn = False
+
 
 while(True):
-    valinta = input("Input direct link to demo or e to exit program: ")
+    query = input("Input direct link to an HLTV demo/Faceit demo/ESEA match page or e to exit program: ")
 
-    if valinta=="1":
+    if query=="e":
         web.quit()
         break
 
-    nimi = input("Name of the match: ")
-    lahde = input("esea or hltv e/h: ")
-    os.mkdir(outDirectory+nimi)
+    matchName = input("Name of the match: ")
+    os.mkdir(outDirectory+matchName)
     
-    if lahde == "e":
-        web = Browser()        
-        web.go_to(valinta)
-        web.click("Katso Demo")        
-        web.driver.minimize_window()
-        eseaNumero = valinta.split("/")
-        eseaNumero = eseaNumero[-1]
-        print(downloadsFolder+"esea_match_"+eseaNumero+".zip")
-       
-        while not os.path.exists(downloadsFolder+"esea_match_"+eseaNumero+".zip"):
-            sleep(1)
-        patoolib.extract_archive(downloadsFolder+"esea_match_"+eseaNumero+".zip",outdir=outDirectory+nimi)
-        web.close_current_tab()
+    if "esea" in query:
+
+        try:
+            web = Browser()
+            webOn = True;
+            web.go_to(query)
+            web.click("Katso Demo")        
+            web.driver.minimize_window()
+            eseaNumber = query.split("/")
+            eseaNumber = eseaNumber[-1]
+            print(downloadsFolder+"esea_match_"+eseaNumber+".zip")
+           
+            while not os.path.exists(downloadsFolder+"esea_match_"+eseaNumber+".zip"):
+                sleep(1)
+            patoolib.extract_archive(downloadsFolder+"esea_match_"+eseaNumber+".zip",outdir=outDirectory+matchName)
+            #web.close_current_tab()
+        except:
+            print("Failed to fetch demo or extract demo. Is the link valid ESEA match link? Also make sure unrar.exe is in the same folder as this program.")
         
               
-    elif lahde == "h":
+    elif "hltv" in query:
         try:
             
             
-            hltvFile = requests.get(valinta)
+            hltvFile = requests.get(query)
             open(downloadsFolder + "\\demo.rar", "wb").write(hltvFile.content)
-            patoolib.extract_archive(downloadsFolder+"demo.rar", outdir=outDirectory+nimi)
+            patoolib.extract_archive(downloadsFolder+"demo.rar", outdir=outDirectory+matchName)
         except:
             print("Something went wrong. Perhaps there is already a match with the same name?")
 
+    elif "faceit" in query:
+        try:
+            faceitFile = requests.get(query)
+            open(downloadsFolder + "\\demo.gz","wb").write(faceitFile.content)
+            with gzip.open(downloadsFolder + "\\demo.gz", 'r') as f_in, open(outDirectory+matchName+"\\"+matchName+".dem", 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+            
+           
+
+        except Exception as e:
+            print(e)
+
     else:
-        print("You didn't choose e or h!")
+        print("Only HLTV or ESEA demos supported currently!")
 
-        
-            #print("Something went wrong. Perhaps there is already a match with the same name?")
-            
-            
-            
-            
+                             
     if(noesisInUse):
-        noesikseen = input("Upload to Noesis y/n :")
+        toNoesis = input("Upload to Noesis y/n :")
 
-        if noesikseen=="y":
-            
+        if toNoesis=="y":
+            if not webOn:
+                web = Browser() 
             
             web.go_to('noesis.gg/app/')
             web.type(noesisUser , into='Username')
             web.type(noesisPW , into='Password')
             web.click('Sign in')
             web.go_to('noesis.gg/app/#/console/upload/')
-            
             sleep(2)
-            filuname = os.listdir(outDirectory+nimi)[0]
-            web.driver.find_element_by_xpath("//input[@name = 'demo-upload-file']").send_keys(outDirectory + nimi + "\\" + filuname)
+            filename = os.listdir(outDirectory+matchName)[0]
+            web.driver.find_element_by_xpath("//input[@name = 'demo-upload-file']").send_keys(outDirectory + matchName + "\\" + filename)
             web.click("UPLOAD")
             web.driver.minimize_window()
             while not web.exists("Analysis complete!"):
@@ -88,4 +103,4 @@ while(True):
         
         
 
-    #C:\\Users\\Jere\\Documents\\  C:\\Users\\Jere\\Downloads\\demo.rar
+
